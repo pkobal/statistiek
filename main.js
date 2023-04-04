ecb18b15e17bfa78aa34f66bde18a0be2d677b20
Patryk Obal <302958475@student.rocmondriaan.nl>
2023-04-04 11:57:59 +0200

    tot opd 3 gemaakt

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 console.log("main.js loaded 🎉");
+
+//opdracht 1
+
+const numbers = [15, 14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+
+let gemiddelde = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    let myNumber = numbers[i];
+    gemiddelde += myNumber;
+}
+
+gemiddelde = gemiddelde/numbers.length
+
+console.log(gemiddelde);
+
+//opdracht 2
+
+const numbers2 = [321, 124, 254, 66, 127, 189, 90, 101, 611, 212, 313, 184, 315];
+
+let gemiddelde2 = 0;
+
+for (let i = 0; i < numbers2.length; i++) {
+    let myNumber2 = numbers2[i];
+    gemiddelde2 += myNumber2;
+}
+
+gemiddelde2 = gemiddelde2/numbers2.length
+
+console.log(gemiddelde2);
+
+//opdracht 3
+
+const numbers3 = [15, 14, 31, 14, 5, 26, 7, 48, 29, 11, 13, 14, 35];
+
+let biggestNumber = 0;
+
+for (let i = 0; i < numbers3.length; i++) {
+    let myNumber3 = numbers3[i];
+    
+}
